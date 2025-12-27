88a1053174b0c54020824b3d47b0933d483d848a
kvasudeva24 <vasudevakartik@gmail.com>
2025-12-26 19:35:50 -0600

    added API functions need to implement

---
+++ openmap.hpp
@@ -0,0 +1,58 @@
+#include <algorithm>
+#include <optional>
+
+
+template<typename K, typename V>
+class OpenMap{
+    
+    struct Node{
+        K key;
+        V value;
+
+        Node() = default;
+    }
+
+
+private:
+    std::optional<Node>* data_ = nullptr;
+    size_t cap_ = 10;
+    size_t size_ = 0;
+    static constexpr double alpha = 0.6;
+
+public:
+    OpenMap(){
+        data_ = new std::optional<Node>[cap_]{};
+    }
+
+
+    void insert(K key, V value){
+
+
+
+
+
+    }
+
+    void remove(K key, V value){
+
+
+
+    }
+
+    V* find(K key){
+
+
+
+    }
+
+
+    size_t capacity() const { return cap_; };
+    size_t size() const { return size_; };
+
+
+
+
+
+
+
+};
